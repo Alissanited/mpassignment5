b2034530cdbbc3be0b140bade621952b5196ded8
Alissanited <alissani09@gmail.com>
2021-04-13 23:20:32 -0700

    4

--- assignment5/src/App.js
+++ assignment5/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Icecream from './order'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="page">
+      <div class="Icecream">
+        <div class="Icecream 1">
+          <Icecream
+          Flavor="Vanilla"
+          Description="When you dig into this pint, you’ll find a rich, creamy Vanilla that’s more vanilla-tasting than any Vanilla you’ve ever tasted."></Icecream>
+        </div>
+        <div class="Icecream 2">
+          <Icecream
+          Flavor="Strawberry"
+          Description="We introduce sweet summer strawberries to pure cream and other natural ingredients. Because it's brimming with real fruit, the true flavor of our strawberries comes shining through."></Icecream>
+        </div>
+        <div>
+          <Icecream
+          Flavor="Chocolate"
+          Description="Rich, creamy, and totally indulgent. Made from the finest cocoa and pure, sweet cream, our chocolate ice cream is the ultimate experience."></Icecream>
+        </div>
+      </div>
     </div>
+        
   );
 }
-
-export default App;

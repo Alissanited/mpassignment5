800b4c97d7aa924ef8c564ae14e3f626b677e843
Alissanited <alissani09@gmail.com>
2021-04-13 18:29:17 -0700

    2

--- assignment5/src/state.js
+++ assignment5/src/state.js
@@ -1,19 +1,21 @@
 import { useState, useEffect} from 'react'
 const [count, setCount] = useState(0)
 
-useEffect(( => {
+useEffect(() => {
     if(count === 100) {
     setTimeout(() => window.open(), 50);
     setTimeout(() => setCount(0), 100);
     }
-} [count])
+}, [count])
 
 return (
     <div className="spin">
         <div style={color}>
             {count}
         </div>
-        <img src= 
+        <img src="https://github.com/Alissanited/mpassignment5/blob/master/assignment5/ice%20cream.png"
+            alt="ice"
+            style={rotate}
             onClick={() => {
                 setCount(count+1)
             }}/>
